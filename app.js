1cdce75b482b546e2f7b29dd9d4ea7934cc4af5b
dpapadat94 <102779032+dpapadat94@users.noreply.github.com>
2023-01-12 17:37:51 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// TODO: Enable strict mode
+"use strict";
+// TODO: Fix the following parameter list
+function parseToJSON(data, data2) {
+  try {
+    // TODO: Add a try/catch block to
+    // attempt to convert 'data' to JSON
+    return JSON.parse(data, data2);
+
+    // TODO: if an exception is raised
+    // print the error to the console
+    // and return null
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+let failData = "<h1>Hello World!</h1>";
+
+let passData = JSON.stringify({ success: true });
+
+console.log(parseToJSON(failData));
+
+console.log(parseToJSON(passData));
